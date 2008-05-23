b598c005deb2f99e44218b06cee94a8b39707e9c
Ryan Berdeen <ryan@ryanberdeen.com>
2008-05-23 13:01:44 -0700

    using renderMain, added license comments

--- src/main/java/com/ryanberdeen/veneer/struts/VeneerResult.java
+++ src/main/java/com/ryanberdeen/veneer/struts/VeneerResult.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2008 Ryan Berdeen.
+ *
+ * This file is part of Veneer.
+ *
+ * Veneer is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Veneer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Veneer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ryanberdeen.veneer.struts;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +39,7 @@ public class VeneerResult extends StrutsResultSupport implements Result {
 		HttpServletResponse response = ServletActionContext.getResponse();
 
 		RenderContext renderContext = VeneerSupport.getContext(ServletActionContext.getServletContext(), request);
-		renderContext.setTemplateName(renderContext.getMainTemplateName());
-		String value = renderContext.render(request, response, renderContext.resolveViewPath(name));
+		String value = renderContext.renderMain(response, name);
 
 		if (value != null) {
 			response.getWriter().write(value);
